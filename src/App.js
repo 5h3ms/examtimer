516e0eaae2c3edaf6b850b3fb85dddb0fa4cedd3
GL65\P53 <5h3ms@pm.me>
2020-12-21 20:19:46 +0500

    added second timer component

--- src/App.js
+++ src/App.js
@@ -1,53 +1,12 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import Timer from './components/Timer.react';
+import React from 'react';
 
 function App() {
-  const calculateTimeLeft = () => {
-    let year = new Date().getFullYear();
-    const difference = +new Date(`12/29/${year}`) - +new Date();
-    let timeLeft = {};
-
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    }
-
-    return timeLeft;
-  };
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-    // Clear timeout if the component is unmounted
-    return () => clearTimeout(timer);
-  });
-
-  const timerComponents = [];
-
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
-
-    timerComponents.push(
-      <span>
-        {timeLeft[interval]} {interval}{' '}
-      </span>,
-    );
-  });
-
   return (
     <div>
-      <h1>HacktoberFest 2020 Countdown</h1>
-      <h2>With React Hooks!</h2>
-      {timerComponents.length ? timerComponents : <span>Times up!</span>}
+      <Timer />
+      <Timer />
     </div>
   );
 }
--- src/components/Timer.react.js
+++ src/components/Timer.react.js
@@ -1,3 +1,55 @@
+import './Timer.css';
+import React, { useEffect, useState } from 'react';
+
 function Timer() {
-    return <div>Test</div>;
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    const difference = +new Date(`12/29/${year}`) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    // Clear timeout if the component is unmounted
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{' '}
+      </span>,
+    );
+  });
+
+  return (
+    <div>
+      <h1>HacktoberFest 2020 Countdown</h1>
+      <h2>With React Hooks!</h2>
+      {timerComponents.length ? timerComponents : <span>Times up!</span>}
+    </div>
+  );
 }
+
+export default Timer;
