a36d071f8bacfdc9b8c31368b7045e939e4bd8af
GL65\P53 <5h3ms@pm.me>
2020-12-21 20:03:47 +0500

    formatting by prettier

--- src/App.js
+++ src/App.js
@@ -1,56 +1,54 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  
-    const calculateTimeLeft = () => {
-      let year = new Date().getFullYear();
-      const difference = +new Date(`12/29/${year}`) - +new Date();
-      let timeLeft = {};
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    const difference = +new Date(`12/29/${year}`) - +new Date();
+    let timeLeft = {};
 
-      if (difference > 0) {
-        timeLeft = {
-          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-          minutes: Math.floor((difference / 1000 / 60) % 60),
-          seconds: Math.floor((difference / 1000) % 60)
-          };
-      }
-
-    return timeLeft;
-
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
     }
 
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+    return timeLeft;
+  };
 
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setTimeLeft(calculateTimeLeft());
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
     }, 1000);
     // Clear timeout if the component is unmounted
     return () => clearTimeout(timer);
-    });
+  });
 
-    const timerComponents = [];
+  const timerComponents = [];
 
-    Object.keys(timeLeft).forEach((interval) => {
-      if (!timeLeft[interval]) {
-        return;
-      }
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
 
-      timerComponents.push(
-        <span>
-          {timeLeft[interval]} {interval}{" "}
-        </span>
-      );
-    });
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{' '}
+      </span>,
+    );
+  });
 
-    return (
-      <div>
-        <h1>HacktoberFest 2020 Countdown</h1>
-        <h2>With React Hooks!</h2>  
+  return (
+    <div>
+      <h1>HacktoberFest 2020 Countdown</h1>
+      <h2>With React Hooks!</h2>
       {timerComponents.length ? timerComponents : <span>Times up!</span>}
-   </div>  
+    </div>
   );
 }
 
