42c292606bf6b49acd98cd5b874f27a710ac46f2
GL65\P53 <5h3ms@pm.me>
2020-12-21 19:25:51 +0500

    Закопипастенные куски кода которые я частично понял, но схема работает. Теперь надо научиться вводить в функцию время до которого оно будет каунтдаунить

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p> Hello, world </p>
-        <Button color="secondary">Hello World</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    const calculateTimeLeft = () => {
+      let year = new Date().getFullYear();
+      const difference = +new Date(`12/29/${year}`) - +new Date();
+      let timeLeft = {};
+
+      if (difference > 0) {
+        timeLeft = {
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+          minutes: Math.floor((difference / 1000 / 60) % 60),
+          seconds: Math.floor((difference / 1000) % 60)
+          };
+      }
+
+    return timeLeft;
+
+    }
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    // Clear timeout if the component is unmounted
+    return () => clearTimeout(timer);
+    });
+
+    const timerComponents = [];
+
+    Object.keys(timeLeft).forEach((interval) => {
+      if (!timeLeft[interval]) {
+        return;
+      }
+
+      timerComponents.push(
+        <span>
+          {timeLeft[interval]} {interval}{" "}
+        </span>
+      );
+    });
+
+    return (
+      <div>
+        <h1>HacktoberFest 2020 Countdown</h1>
+        <h2>With React Hooks!</h2>  
+      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+   </div>  
   );
 }
 
