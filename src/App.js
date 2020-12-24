8edd0aa85a2b8ff8235b060d60724c8c7bd6bfe5
Rustam Ganeyev <rganeyev@gmail.com>
2020-12-24 19:32:26 +0000

    added "add timer" button

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
 import './App.css';
 import Timer from './components/Timer.react';
-import React, {useState} from 'react';
-import { Switch, Grid, Paper, Typography, Card} from '@material-ui/core';
-import { ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import React, { useState, useCallback } from 'react';
+import {
+  Switch,
+  Grid,
+  Paper,
+  Typography,
+  Card,
+  Button,
+} from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
   const theme = createMuiTheme({
     palette: {
-      type: darkMode ? "dark" : "light",
-    }
-
+      type: darkMode ? 'dark' : 'light',
+    },
   });
 
+  const defaultTime = { minutes: 5 };
+  const [timers, setTimers] = useState([<Timer {...defaultTime} key={0} />]);
+
+  const addTimer = useCallback(() => {
+    const id = timers.length;
+    setTimers((timers) => [...timers, <Timer {...defaultTime} key={id} />]);
+  }, [timers.length]);
 
   return (
-    <ThemeProvider theme ={theme}>
-      <Paper style={{ height: "100vh" }}>
-      <Typography variant="h3" >Centre number RU105</Typography>
-      
-      <Grid container spacing={5} justify="center">
-        <Grid item xs={6}><Card variant="outlined"><Typography variant="h5">Mathematics paper 1</Typography><Timer minutes={5} /></Card></Grid>
-        <Grid item xs={6}><Card variant="outlined"><Typography variant="h5">Science paper 2</Typography><Timer days={1} hours={2} minutes={5} seconds={3} /></Card></Grid>
-      </Grid>
-        <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} /> 
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: '100vh' }}>
+        <Typography variant="h3">Centre number RU105</Typography>
+        <Button variant="contained" color="primary" onClick={addTimer}>
+          Add timer
+        </Button>
+        <Grid container spacing={5} justify="center">
+          {timers.map((timer, index) => (
+            <Grid item xs={6} key={index}>
+              <Card variant="outlined">
+                <Typography variant="h5">Timer {index}</Typography>
+                {timer}
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
       </Paper>
-    
-     
     </ThemeProvider>
   );
 }
