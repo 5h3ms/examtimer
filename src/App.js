3f009db3e69278ce642d9dd36ebc6f5da16d40dd
GL65\P53 <5h3ms@pm.me>
2020-12-24 20:23:37 +0500

    added material grid and dark theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
 import Timer from './components/Timer.react';
-import React from 'react';
+import React, {useState} from 'react';
+import { Switch, Grid, Paper, Typography, Card} from '@material-ui/core';
+import { ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    }
+
+  });
+
+
   return (
-    <div>
-      <h1>HacktoberFest 2020 Countdown</h1>
-      <h2>With React Hooks!</h2>
-      <Timer minutes={5} />
-      <Timer days={1} hours={2} minutes={5} seconds={3} />
-    </div>
+    <ThemeProvider theme ={theme}>
+      <Paper style={{ height: "100vh" }}>
+      <Typography variant="h3" >Centre number RU105</Typography>
+      
+      <Grid container spacing={5} justify="center">
+        <Grid item xs={6}><Card variant="outlined"><Typography variant="h5">Mathematics paper 1</Typography><Timer minutes={5} /></Card></Grid>
+        <Grid item xs={6}><Card variant="outlined"><Typography variant="h5">Science paper 2</Typography><Timer days={1} hours={2} minutes={5} seconds={3} /></Card></Grid>
+      </Grid>
+        <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} /> 
+      </Paper>
+    
+     
+    </ThemeProvider>
   );
 }
 
